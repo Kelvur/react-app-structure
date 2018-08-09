ce4684ab8071f3badbe9820d7b4058d1e1738620
Miguel Osorio Orodea <Osorio10.92@gmail.com>
2018-08-09 23:43:45 +0200

    Init example of react-router-dom

--- src/01-containers/app/index.js
+++ src/01-containers/app/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 // Components
 import Button from '00-components/button';
+import {Link} from 'react-router-dom';
 // Style
 import 'style/style.css';
 
@@ -12,7 +13,10 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<h1>App Here!</h1>
-				<Button>This button does nothing!</Button>
+				<ul>
+					<li><Button>This button does nothing!</Button></li>
+					<li><Link to='/about'>Go to About</Link></li>
+				</ul>
 			</div>
 		)
 	}
--- src/main.js
+++ src/main.js
@@ -4,15 +4,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 // Store
 import store from '04-reducers/00-store/createStore';
 // Container
 import App from '01-containers/app';
+import About from '01-containers/about'
 
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App/>
+		<BrowserRouter>
+			<div>
+				<Route path='/' exact component={App}/>
+				<Route path='/about' component={About}/>
+			</div>
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById('app')
 );
