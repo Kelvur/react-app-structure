9fa6258a3feaa5d496bd2b49a329f23864a343ad
Kelvur <Osorio10.92@gmail.com>
2018-10-28 19:27:49 +0100

    Added more style to the header, first version of the header finished

--- src/01-containers/header/index.js
+++ src/01-containers/header/index.js
@@ -1,8 +1,9 @@
 // Core
 import React from 'react'
+// Components
+import SelectLocale from '00-components/styledSelectLocale'
 // Containers
 import {i18n} from '01-containers/i18n'
-import SelectLocale from '01-containers/selectLocale'
 
 
 @i18n()
@@ -11,8 +12,10 @@ export default class HeaderContainer extends React.PureComponent {
 	render(){
 		return (
 			<header className="app-header">
-				<span>{this.props.locale.APP_TITLE}</span>
-				<SelectLocale/>
+				<span className="app-header__title">{this.props.locale.APP_TITLE}</span>
+				<div className="locale-box">
+					<SelectLocale/>
+				</div>
 			</header>
 		)
 	}
