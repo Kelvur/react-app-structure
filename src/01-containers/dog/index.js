af83803b3b754e689efea742f8ffcca9930a0e93
Kelvur <Osorio10.92@gmail.com>
2018-11-18 16:41:42 +0100

    Added renderImg for Cat and Dog page, now the images are displayed on the web

--- src/01-containers/dog/index.js
+++ src/01-containers/dog/index.js
@@ -3,11 +3,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 // Components
 import {Link} from 'react-router-dom'
+// Containers
+import {i18n} from '01-containers/i18n'
 // Actions
 import {getRandomDogUrl} from '02-actions/dog/DispatchFunctions'
 // Constants
 import {HOME} from '05-constants/router'
+// Style
+import './style.less'
 
+
+function renderImg(url, className = 'dog__img'){
+	return url ? <img className={className} src={url} title="Dog picture"/> : null
+}
 
 function mapStateToProps(state){
 	return {
@@ -18,10 +26,30 @@ function mapStateToProps(state){
 @connect(mapStateToProps, {
 	getRandomDogUrl,
 })
+@i18n()
 export default class DogContainer extends React.Component {
 
 	componentDidMount(){
-		this.props.getRandomDogUrl();
+		this.bindReload()
+		this.props.getRandomDogUrl()
+	}
+
+	componentWillUnmount(){
+		this.unbindReload()
+	}
+
+	bindReload(){
+		window.addEventListener('keypress', this.handleKeypressReload)
+	}
+
+	unbindReload(){
+		window.removeEventListener('keypress', this.handleKeypressReload)
+	}
+
+	handleKeypressReload = evt => {
+		if(evt.key === 'r' || evt.key === 'R'){
+			this.props.getRandomDogUrl()
+		}
 	}
 
 	renderUrl(url){
@@ -30,10 +58,11 @@ export default class DogContainer extends React.Component {
 
 	render(){
 		return (
-			<div>
-				<h2>Dog</h2>
-				{this.renderUrl(this.props.url)}
-				<Link to={HOME}>Go back to Home</Link>
+			<div className="dog">
+				<h2>{this.props.locale.DOG_PAGE}</h2>
+				{renderImg(this.props.url, this.props.locale.DOG_IMG_TITLE)}
+				<h5 className="dog__link">{this.renderUrl(this.props.url)}</h5>
+				<Link to={HOME}>{this.props.locale.BACK_HOME}</Link>
 			</div>
 		)
 	}
--- src/05-constants/locale/es-ES/index.js
+++ src/05-constants/locale/es-ES/index.js
@@ -1,6 +1,8 @@
 export default {
 	APP_TITLE: 'Bienvenido a React-App-Structure',
+	BACK_HOME: 'Volver al inicio',
 	CAT_PAGE: 'Página del Gato',
+	DOG_PAGE: 'Página del Perro',
 }
 
 export const ES_ES_NUMBER_FORMAT = {
